698a20f159ecda15dc1345526618138e1dca7c58
Edward4j <eduard.dovgal@gmail.com>
2018-08-03 11:59:11 +0300

    Create package.json file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 angular.module('flapperNewsFront', [
   // Include external module 'ui-router' as a dependency in our app:
   'ui.router'
+  // 'templates'
 ])
   // Create the config block for our app and configure a home state using $stateProvider and $urlRouterProvider. Use otherwise() to redirect unspecified routes.
   .config([
